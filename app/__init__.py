e58d16dc5de54ea1bb1617e919c835c218952124
Elaine Watkins <ewatki.ga@gmail.com>
2023-05-11 18:26:07 -0400

    fixed syntax errors

--- app/__init__.py
+++ app/__init__.py
@@ -16,10 +16,10 @@ def create_app(test_config=None):
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
             "SQLALCHEMY_DATABASE_URI")
     else:
-        app.config["TESTING"] = True
-        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_TEST_DATABASE_URI")
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            'SQLALCHEMY_TEST_DATABASE_URI')
 
     db.init_app(app)
     migrate.init_app(app, db)
