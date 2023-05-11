6e6f0c90e9f3ffe2a2a59b557e51dbb39c043dd4
Elaine Watkins <ewatki.ga@gmail.com>
2023-05-11 14:45:49 -0400

    created .env file, modified create_app function to handle test mode

--- app/routes.py
+++ app/routes.py
@@ -4,22 +4,6 @@ from app import db
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-# @books_bp.route("", methods=["GET", "POST"])
-# def handle_books():
-#     if request.method == "GET":
-#         books = Book.query.all()
-#         books_response = []
-#         for book in books:
-#             books_response.append({
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             })
-#         return jsonify(books_response)
-#     elif request.method == "POST":
-#         # ... Indent all of the Create Book functionality into this elif
-#         # request_body = request.get_json()
-
 @books_bp.route("", methods=["POST"])
 def handle_books():
     request_body = request.get_json()
