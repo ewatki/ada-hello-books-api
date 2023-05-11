4079dc3a5d1c4359cb4c48407855a1b7b16fa574
Elaine Watkins <ewatki.ga@gmail.com>
2023-05-11 13:28:44 -0400

    added book endpoint to create book

--- app/routes.py
+++ app/routes.py
@@ -1,76 +1,16 @@
-from flask import Blueprint, jsonify, abort, make_response
-
-# class Book:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
-
-# books = [
-#     Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-#     Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-# ] 
+from flask import Blueprint, jsonify, abort, make_response, request
+from app.models.book import Book
+from app import db
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+@books_bp.route("", methods=["POST"])
+def handle_books():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
 
-# def validate_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except:
-#         abort(make_response({"message":f"book {book_id} invalid"}, 400))
+    db.session.add(new_book)
+    db.session.commit()
 
-#     for book in books:
-#         if book.id == book_id:
-#             return book
-
-#     abort(make_response({"message":f"book {book_id} not found"}, 404))
-
-# @books_bp.route("", methods=["GET"])
-# def handle_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append({
-#             "id": book.id,
-#             "title": book.title,
-#             "description": book.description
-#         })
-#     return jsonify(books_response)
-
-# @books_bp.route("/<book_id>", methods=["GET"])
-# def handle_book(book_id):
-#     book = validate_book(book_id)
-
-#     return {
-#         "id": book.id,
-#         "title": book.title,
-#         "description": book.description,
-#     }
-
-# hello_world_bp = Blueprint("hello_world_bp", __name__, )
-
-# @hello_world_bp.route("/hello_world", methods=["GET"])
-# def say_hello_world():
-#     my_beautiful_response_body = "Hello, World!"
-#     return my_beautiful_response_body
-
-# @hello_world_bp.route("/hello/JSON", methods=["GET"])
-# def say_hello_json():
-#     return {
-#     "name": "Ada Lovelace",
-#     "message": "Hello!",
-#     "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-#     }
-
-
-# @hello_world_bp.route("/broken-endpoint-with-broken-server-code")
-# def broken_endpoint():
-#     response_body = {
-#         "name": "Ada Lovelace",
-#         "message": "Hello!",
-#         "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-#     }
-#     new_hobby = "Surfing"
-#     response_body["hobbies"] + new_hobby
-#     return response_body
+    return make_response(f"Book {new_book.title} successfully created", 201)
