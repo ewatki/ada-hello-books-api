4087335d96d565235c22f144878ca2a8b247f154
Elaine Watkins <ewatki.ga@gmail.com>
2023-05-11 13:37:35 -0400

    added route to read all books

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,22 @@ from app import db
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+# @books_bp.route("", methods=["GET", "POST"])
+# def handle_books():
+#     if request.method == "GET":
+#         books = Book.query.all()
+#         books_response = []
+#         for book in books:
+#             books_response.append({
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description
+#             })
+#         return jsonify(books_response)
+#     elif request.method == "POST":
+#         # ... Indent all of the Create Book functionality into this elif
+#         # request_body = request.get_json()
+
 @books_bp.route("", methods=["POST"])
 def handle_books():
     request_body = request.get_json()
@@ -14,3 +30,17 @@ def handle_books():
     db.session.commit()
 
     return make_response(f"Book {new_book.title} successfully created", 201)
+
+@books_bp.route("", methods=["GET"])
+def read_all_books():
+    books_response = []
+    books = Book.query.all()
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return jsonify(books_response)
