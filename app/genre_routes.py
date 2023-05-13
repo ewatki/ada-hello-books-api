a3e517c1acb24e90689349b58e3faac1c1f7169b
Elaine Watkins <ewatki.ga@gmail.com>
2023-05-13 16:00:43 -0400

    created nested routes to read and create genres for books

--- app/genre_routes.py
+++ app/genre_routes.py
@@ -1,5 +1,6 @@
 from app import db
 from app.models.genre import Genre
+from app.models.book import Book
 from app.book_routes import validate_model
 from flask import Blueprint, jsonify, abort, make_response, request
 
@@ -17,7 +18,6 @@ def create_genre():
 
 @genres_bp.route("", methods=["GET"])
 def read_all_genres():
-    
     genres = Genre.query.all()
 
     genres_response = []
@@ -28,3 +28,37 @@ def read_all_genres():
             }
         )
     return jsonify(genres_response)
+
+
+# Nested Routes : Create a new book with its genre (and author)
+@genres_bp.route("/<genre_id>/books", methods=["POST"])
+def create_genre_for_book(genre_id): 
+    # Query the Genre table to get the genre with genre_id
+    genre = validate_model(Genre, genre_id)
+    request_body = request.get_json()
+    # Create a new book instance with data from the request_body and the genre
+    new_book = Book(
+        title=request_body["title"],
+        description=request_body["description"],
+        author_id=request_body["author_id"],
+        # We get the genre(s) from input
+        genres=[genre]
+    )
+
+    # Commit our new book from the database
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(jsonify(f"Book {new_book.title} by {new_book.author.name} successfully created"), 201)
+
+@genres_bp.route("/<genre_id>/books", methods=["GET"])
+def read_all_genres_by_book(genre_id): 
+    # Query the Genre table to get the genre with genre_id
+    genre = validate_model(Genre, genre_id)
+    # Iterate through the books with that genre
+    books_response = []
+    for book in genre.books: 
+        # genre.books returns a list of Book instances associated with the Genre instance named genre
+        books_response.append(book.to_dict())
+    # Return a response as a list of dictionaries with information for each book in the specified genre.
+    return jsonify(books_response)
